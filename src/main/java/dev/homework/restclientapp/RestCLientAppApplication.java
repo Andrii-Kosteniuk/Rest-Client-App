f1c031aede46dc85f270cd3c391a750209ce20f1
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-02-17 14:29:00 +0100

    Initial commit

---
+++ src/main/java/dev/homework/restclientapp/RestCLientAppApplication.java
@@ -0,0 +1,13 @@
+package dev.homework.restclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCLientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCLientAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/homework/restclientapp/RestCLientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.homework.restclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCLientAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
