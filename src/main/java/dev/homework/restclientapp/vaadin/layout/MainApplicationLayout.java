fd8869ab97f1d67af8708b2abfa1f89f2718f44b
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-02-23 17:56:17 +0100

    Changed names of variables and improved logic of defining tabs

--- src/main/java/dev/homework/restclientapp/vaadin/layout/MainApplicationLayout.java
+++ src/main/java/dev/homework/restclientapp/vaadin/layout/MainApplicationLayout.java
@@ -1,29 +1,32 @@
 package dev.homework.restclientapp.vaadin.layout;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 
-@Route("/home")
+import java.util.ArrayList;
+import java.util.List;
+
+@Route("")
 public class MainApplicationLayout extends AppLayout {
 
     public MainApplicationLayout() {
         DrawerToggle toggle = new DrawerToggle();
 
-        H1 title = new H1("Car pocket");
-        title.getStyle()
+        H1 mainTitle = new H1("Vehicle pocket");
+        mainTitle.getStyle()
                 .set("font-size", "var(--lumo-font-size-l)")
                 .set("margin", "0");
 
         Tabs tabs = getTabs();
 
         addToDrawer(tabs);
-        addToNavbar(toggle, title);
+        addToNavbar(toggle, mainTitle);
 
         setPrimarySection(Section.DRAWER);
     }
@@ -31,14 +34,25 @@ public class MainApplicationLayout extends AppLayout {
     private Tabs getTabs() {
         Tabs tabs = new Tabs();
 
-        Component car = new Button("Find cars", event -> getUI().ifPresent(ui -> ui.navigate("/car-search")));
-        Tab cars = new Tab(car);
-        Tab about = new Tab("About");
+        Button vehicleFindButton = new Button("Find Vehicles", event -> getUI().ifPresent(ui -> ui.navigate("/vehicles-search")));
+        vehicleFindButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        Tab findVehicleTab = new Tab(vehicleFindButton);
 
-        tabs.add(cars, about);
+        List<Tab> tabsList = defineTabs();
+        tabsList.forEach(tabs::add);
+
+        tabs.add(findVehicleTab);
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         tabs.setHeight("240px");
         tabs.setWidth("240px");
+        return tabs;
+    }
+
+    private List<Tab> defineTabs() {
+        List<Tab> tabs = new ArrayList<>();
+        tabs.add(new Tab("Home"));
+        tabs.add(new Tab("About"));
+        tabs.add(new Tab("Contact"));
         return tabs;
     }
 }
