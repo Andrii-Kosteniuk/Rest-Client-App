8ed77cb9c7fc3fd584a3153fda0ec603adf63653
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-04 16:04:23 +0100

    Separated the validating logic to another specific class. Added notifications if a user sends a request with incorrect data

--- src/main/java/dev/homework/restclientapp/vaadin/view/SearchCarView.java
+++ src/main/java/dev/homework/restclientapp/vaadin/view/SearchCarView.java
@@ -1,21 +1,28 @@
 package dev.homework.restclientapp.vaadin.view;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import dev.homework.restclientapp.dto.request.VehicleRequest;
 import dev.homework.restclientapp.dto.response.general.VehicleMainRecord;
 import dev.homework.restclientapp.dto.response.singleVehicle.VehicleByIdRecords;
 import dev.homework.restclientapp.service.ProvinceService;
 import dev.homework.restclientapp.service.VehicleService;
+import dev.homework.restclientapp.util.DataValidation;
 import dev.homework.restclientapp.vaadin.layout.MainApplicationLayout;
 
 import java.lang.reflect.Field;
@@ -25,15 +32,13 @@ import java.util.List;
 
 @Route(value = "/vehicles-search", layout = MainApplicationLayout.class)
 public class SearchCarView extends Div {
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
     private final VehicleService vehicleService;
     private final ProvinceService cepikService;
-    private final Grid<VehicleMainRecord> carGrid = new Grid<>(VehicleMainRecord.class);
+    private final Grid<VehicleMainRecord> mainRecordGrid = new Grid<>(VehicleMainRecord.class);
     private final Select<String> selectProvince = new Select<>();
     private final DatePicker datePickerFrom = new DatePicker("Data od");
     private final DatePicker datePickerTo = new DatePicker("Data do");
-    private final Binder<VehicleRequest> binder = new Binder<>(VehicleRequest.class);
-
-
     VehicleRequest vehicleRequest = new VehicleRequest();
 
     public SearchCarView(VehicleService vehicleService, ProvinceService provinceService) {
@@ -50,17 +55,38 @@ public class SearchCarView extends Div {
         formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
                 new FormLayout.ResponsiveStep("500px", 3));
 
+        DataValidation.validateDates(datePickerFrom, datePickerTo);
+        DataValidation.validateProvinceNameIsNotEmpty(selectProvince);
 
-        add(formLayout, carGrid);
+        add(formLayout, mainRecordGrid);
         showTableVehicleInformation();
 
-        configureValidation();
+    }
+
+    private static void showValidationErrorNotification() {
+        Notification errorValidationNotification = new Notification();
+        errorValidationNotification.addThemeVariants(NotificationVariant.LUMO_WARNING);
+
+        Div text = new Div(new Text("Please, check if you are providing a correct date!"));
+
+        Button closeButton = new Button(new Icon("lumo", "cross"));
+        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        closeButton.setAriaLabel("Close");
+        closeButton.addClickListener(event -> errorValidationNotification.close());
+
+        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        errorValidationNotification.add(layout);
+        errorValidationNotification.open();
     }
 
     private void submitForm() {
-        if (binder.validate().isOk()) {
+        if (datePickerFrom.getErrorMessage() == null && datePickerTo.getErrorMessage() == null) {
             fillFormFieldsForSearchVehicles();
-
+            fetchDataFromApi();
+        } else {
+            showValidationErrorNotification();
         }
     }
 
@@ -77,8 +103,9 @@ public class SearchCarView extends Div {
     }
 
     private void showTableVehicleInformation() {
-        carGrid.setColumns("marka", "model", "dataPierwszejRejestracji", "rokProdukcji");
-        carGrid.addItemClickListener(event -> {
+        mainRecordGrid.setColumns("marka", "model", "dataPierwszejRejestracji", "rokProdukcji");
+
+        mainRecordGrid.addItemClickListener(event -> {
             VehicleMainRecord selectedCar = event.getItem();
             if (selectedCar != null) {
                 showCarDetails(selectedCar.getId());
@@ -89,16 +116,17 @@ public class SearchCarView extends Div {
     private void fillFormFieldsForSearchVehicles() {
         vehicleRequest.setProvinceName(cepikService.getProvinceKey(selectProvince.getValue()));
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate dateFrom = datePickerFrom.getValue();
+        LocalDate dateTo = datePickerTo.getValue();
 
-        String dateFrom = datePickerFrom.getValue().format(formatter);
-        String dateTo = datePickerTo.getValue().format(formatter);
+        vehicleRequest.setDateFrom(DATE_FORMATTER.format(dateFrom));
+        vehicleRequest.setDateTo(DATE_FORMATTER.format(dateTo));
 
-        vehicleRequest.setDateFrom(dateFrom);
-        vehicleRequest.setDateTo(dateTo);
+    }
 
+    private void fetchDataFromApi() {
         List<VehicleMainRecord> vehicleMainRecords = vehicleService.getVehicleMainInfo(vehicleRequest);
-        carGrid.setItems(vehicleMainRecords);
+        mainRecordGrid.setItems(vehicleMainRecords);
     }
 
     private void showCarDetails(String vehicleId) {
@@ -131,29 +159,4 @@ public class SearchCarView extends Div {
         detailsDialog.open();
     }
 
-    private void configureValidation() {
-        binder.forField(selectProvince)
-                .asRequired("Wybierz województwo!")
-                .bind(VehicleRequest::getProvinceName, VehicleRequest::setProvinceName);
-
-        binder.forField(datePickerFrom)
-                .asRequired("Wybierz datę początkową!")
-                .withConverter(LocalDate::toString, LocalDate::parse)
-                .bind(VehicleRequest::getDateFrom, VehicleRequest::setDateFrom);
-
-        binder.forField(datePickerTo)
-                .asRequired("Wybierz datę końcową!")
-                .withValidator(date -> {
-                    LocalDate fromDate = datePickerFrom.getValue();
-                    return fromDate == null || date == null || ! fromDate.isAfter(date);
-                }, "Data od nie może być późniejsza niż data do!")
-                .withConverter(LocalDate::toString, LocalDate::parse)
-                .bind(VehicleRequest::getDateTo, VehicleRequest::setDateTo);
-
-        binder.forField(datePickerTo)
-                .withValidator(date -> date == null || ! date.isAfter(LocalDate.now()),
-                        "Data nie może być późniejsza niż dzisiejsza data!")
-                .withConverter(LocalDate::toString, LocalDate::parse)
-                .bind(VehicleRequest::getDateTo, VehicleRequest::setDateTo);
-    }
 }
