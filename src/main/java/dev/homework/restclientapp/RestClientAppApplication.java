791bd00e73d0b31ba80b70ac600240fbe6c066a6
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-07 20:33:12 +0100

    Added @EnableCaching

--- src/main/java/dev/homework/restclientapp/RestClientAppApplication.java
+++ src/main/java/dev/homework/restclientapp/RestClientAppApplication.java
@@ -2,10 +2,10 @@ package dev.homework.restclientapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableRetry
+@EnableCaching
 public class RestClientAppApplication {
 
     public static void main(String[] args) {
