6bf834697ed64e80ca0bdd9cd6517b833cbaf530
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-07 20:31:24 +0100

    Refactored validation logic

--- src/main/java/dev/homework/restclientapp/util/DataValidation.java
+++ src/main/java/dev/homework/restclientapp/util/DataValidation.java
@@ -4,57 +4,53 @@ import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.data.binder.Binder;
 import dev.homework.restclientapp.dto.request.VehicleRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+@Component
 public class DataValidation {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(DataValidation.class);
-    public static final Binder<VehicleRequest> BINDER = new Binder<>();
+    public final Binder<VehicleRequest> binder = new Binder<>(VehicleRequest.class);
 
-    public static void validateDates(DatePicker datePickerFrom, DatePicker datePickerTo) {
-        datePickerFrom.addValueChangeListener(event -> configureValidation(datePickerFrom, datePickerTo));
-        datePickerTo.addValueChangeListener(event -> configureValidation(datePickerFrom, datePickerTo));
-    }
+    public void validateProvinceNameIsNotEmpty(Select<String> provinces) {
 
-    public static void configureValidation(DatePicker datePickerFrom, DatePicker datePickerTo) {
-
-        LocalDate fromDate = datePickerFrom.getValue();
-        LocalDate toDate = datePickerTo.getValue();
-
-        datePickerFrom.setInvalid(false);
-        datePickerTo.setInvalid(false);
-        datePickerFrom.setErrorMessage(null);
-        datePickerTo.setErrorMessage(null);
-
-        if (fromDate != null && toDate != null) {
-            if (fromDate.isAfter(toDate)) {
-                datePickerFrom.setInvalid(true);
-                datePickerFrom.setErrorMessage("Data od nie może być późniejsza niż data do!");
-                LOGGER.warn(" {}", datePickerFrom.getErrorMessage());
-            } else if (toDate.isBefore(fromDate)) {
-                datePickerTo.setInvalid(true);
-                datePickerTo.setErrorMessage("Data do nie może być wcześniejsza niż data od!");
-                LOGGER.warn(" {}", datePickerTo.getErrorMessage());
-            } else if (fromDate.isAfter(LocalDate.now())) {
-                datePickerFrom.setInvalid(true);
-                datePickerFrom.setErrorMessage("Data od nie może być późniejsza niż data dzisiejsza!");
-                LOGGER.warn(" {}", datePickerFrom.getErrorMessage());
-            } else if (toDate.isAfter(LocalDate.now())) {
-                datePickerTo.setInvalid(true);
-                datePickerTo.setErrorMessage("Data do nie może być późniejsza niż data dzisiejsza!");
-                LOGGER.warn(" {}", datePickerTo.getErrorMessage());
-            }
-        }
-    }
-
-    public static void validateProvinceNameIsNotEmpty(Select<String> provinces) {
-
-        BINDER.forField(provinces)
+        binder.forField(provinces)
                 .asRequired("Wybierz województwo!")
                 .bind(VehicleRequest::getProvinceName, VehicleRequest::setProvinceName)
                 .validate();
     }
+
+
+    public void validateDates(DatePicker datePickerFrom, DatePicker datePickerTo) {
+
+        validateDateFrom(datePickerFrom, datePickerTo);
+        validateDateTo(datePickerFrom, datePickerTo);
+    }
+
+    private void validateDateTo(DatePicker datePickerFrom, DatePicker datePickerTo) {
+        binder.forField(datePickerTo)
+                .withValidator(date -> date == null || datePickerFrom.getValue() == null || ! date.isBefore(datePickerFrom.getValue()),
+                        "Data do nie może być wcześniejsza niż data od!")
+                .withValidator(date -> date == null || ! date.isAfter(LocalDate.now()),
+                        "Data nie może być późniejsza niż dzisiejsza data!")
+                .withConverter(LocalDate::toString, LocalDate::parse)
+                .bind(VehicleRequest::getDateTo, VehicleRequest::setDateTo);
+
+        datePickerTo.addValueChangeListener(event -> binder.validate());
+    }
+
+    private void validateDateFrom(DatePicker datePickerFrom, DatePicker datePickerTo) {
+        binder.forField(datePickerFrom)
+                .withValidator(date -> date == null || datePickerTo.getValue() == null || ! date.isAfter(datePickerTo.getValue()),
+                        "Data od nie może być późniejsza niż data do!")
+                .withValidator(date -> date == null || ! date.isAfter(LocalDate.now()),
+                        "Data nie może być późniejsza niż dzisiejsza data!")
+                .withConverter(LocalDate::toString, LocalDate::parse)
+                .bind(VehicleRequest::getDateFrom, VehicleRequest::setDateFrom);
+
+        datePickerFrom.addValueChangeListener(event -> binder.validate());
+    }
+
+
 }
