d3295951e339847e3bf435cbb833bfe4c4c99d98
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-07 20:29:41 +0100

    Added @AllArgsConstructor and       @NoArgsConstructor

--- src/main/java/dev/homework/restclientapp/dto/response/singleVehicle/VehicleByIdResponse.java
+++ src/main/java/dev/homework/restclientapp/dto/response/singleVehicle/VehicleByIdResponse.java
@@ -1,10 +1,14 @@
 package dev.homework.restclientapp.dto.response.singleVehicle;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleByIdResponse {
 
     @JsonProperty("data")
