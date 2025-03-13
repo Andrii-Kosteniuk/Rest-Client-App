81596bbc8d6e0c1c449e44190b48601d37c4b7a2
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-13 19:01:53 +0100

    Refactored code for reducing loose coupling

--- src/main/java/dev/homework/restclientapp/vaadin/view/FilterLayout.java
+++ src/main/java/dev/homework/restclientapp/vaadin/view/FilterLayout.java
@@ -3,17 +3,15 @@ package dev.homework.restclientapp.vaadin.view;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.grid.HeaderRow;
+import com.vaadin.flow.component.grid.contextmenu.GridContextMenu;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -21,57 +19,49 @@ import com.vaadin.flow.router.Route;
 import dev.homework.restclientapp.dto.response.general.VehicleMainRecord;
 import dev.homework.restclientapp.service.VehicleService;
 import dev.homework.restclientapp.vaadin.layout.MainApplicationLayout;
+import dev.homework.restclientapp.vaadin.layout.PaginationLayout;
+import dev.homework.restclientapp.vaadin.layout.SearchFormLayout;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 
 @Route(value = "/vehicles-filter", layout = MainApplicationLayout.class)
-public class FilterView extends VerticalLayout {
+public class FilterLayout extends VerticalLayout {
+    private final VehicleView vehicleView;
 
-
-    public FilterView(VehicleService vehicleService, VehicleInformationView vehicleInformationView) {
-        Grid<VehicleMainRecord> vehicleMainRecordGrid = new Grid<>(VehicleMainRecord.class, false);
-        Grid.Column<VehicleMainRecord> markColumn = vehicleMainRecordGrid.addColumn(VehicleMainRecord::getMarka);
-        Grid.Column<VehicleMainRecord> modelColumn = vehicleMainRecordGrid.addColumn(VehicleMainRecord::getModel);
-        Grid.Column<VehicleMainRecord> yearColumn = vehicleMainRecordGrid.addColumn(VehicleMainRecord::getRokProdukcji);
-        Grid.Column<VehicleMainRecord> registrationDate = vehicleMainRecordGrid.addColumn(VehicleMainRecord::getDataPierwszejRejestracji);
+    public FilterLayout(VehicleService vehicleService, VehicleInformationView vehicleInformationView) {
+        this.vehicleView = new VehicleView(vehicleService);
         int page = vehicleService.getCurrentPage();
 
-        vehicleMainRecordGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        Grid<VehicleMainRecord> vehicleMainRecordGrid = vehicleView.getVehicleMainRecordGrid();
+
+        vehicleMainRecordGrid.setTooltipGenerator(info -> """
+                Kliknij prawym przyciskiem myszy, aby zobaczyć pełne informacje
+                """);
+
 
         Button backButton = new Button("Back", new Icon(VaadinIcon.ARROW_LEFT),
-                event -> UI.getCurrent().navigate(SearchCarView.class));
+                event -> UI.getCurrent().navigate(SearchFormLayout.class));
 
-        HorizontalLayout paginationControls = getHorizontalLayout(vehicleService, page);
+
+        VehicleFilter vehicleFilter = new VehicleFilter(vehicleView.getGridListData());
+
+        addHeaderRow(vehicleView.getMarkColumn(), vehicleFilter, vehicleView.getModelColumn(), vehicleView.getYearColumn(), vehicleView.getRegistrationDate());
+
+        GridContextMenu<VehicleMainRecord> menu = vehicleMainRecordGrid.addContextMenu();
+
+        menu.addItem("Zobacz szegółowe informacje", event -> {
+            Optional<VehicleMainRecord> selectedCar = event.getItem();
+
+            selectedCar.ifPresent(vehicleMainRecord ->
+                    vehicleInformationView.showDialogWithVehicleDetails(vehicleMainRecord.getId()));
+        });
+
+        HorizontalLayout paginationControls = PaginationLayout.getPagesAndSize(vehicleService, page);
         paginationControls.setSizeFull();
 
-        List<VehicleMainRecord> vehicleMainRecords = vehicleService.getCachedData();
-        GridListDataView<VehicleMainRecord> dataView = vehicleMainRecordGrid.setItems(vehicleMainRecords);
-
-
-        VehicleFilter vehicleFilter = new VehicleFilter(dataView);
-        vehicleMainRecordGrid.getHeaderRows().clear();
-
-        HeaderRow headerRow = vehicleMainRecordGrid.appendHeaderRow();
-
-        headerRow.getCell(markColumn).setComponent(
-                createFilterHeader("Marka", vehicleFilter::setMark));
-        headerRow.getCell(modelColumn).setComponent(
-                createFilterHeader("Model", vehicleFilter::setModel));
-        headerRow.getCell(yearColumn).setComponent(
-                createFilterHeader("Rok produkcji", vehicleFilter::setYear));
-        headerRow.getCell(registrationDate).setComponent(
-                createFilterHeader("Data pierwszej rejestracji ", vehicleFilter::setDate));
-
         add(backButton, vehicleMainRecordGrid, paginationControls);
-
-        vehicleMainRecordGrid.addItemClickListener(event -> {
-            VehicleMainRecord selectedCar = event.getItem();
-            if (selectedCar != null) {
-                vehicleInformationView.showDialogWithVehicleDetails(selectedCar.getId());
-            }
-        });
     }
 
     private static Component createFilterHeader(String labelText, Consumer<String> filter) {
@@ -94,35 +84,17 @@ public class FilterView extends VerticalLayout {
         return layout;
     }
 
-    private HorizontalLayout getHorizontalLayout(VehicleService vehicleService, int page) {
-        Button prevButton = new Button("Previous", event -> {
-            if (page > 1) {
-                vehicleService.setCurrentPage(page - 1);
-            }
-            vehicleService.submitForm();
-        });
+    private void addHeaderRow(Grid.Column<VehicleMainRecord> markColumn, VehicleFilter vehicleFilter, Grid.Column<VehicleMainRecord> modelColumn, Grid.Column<VehicleMainRecord> yearColumn, Grid.Column<VehicleMainRecord> registrationDate) {
+        HeaderRow headerRow = vehicleView.getVehicleMainRecordGrid().appendHeaderRow();
 
-        Button nextButton = new Button("Next", event -> {
-            vehicleService.setCurrentPage(page + 1);
-            vehicleService.submitForm();
-        });
-
-        Select<Integer> pageLimitSelect = new Select<>();
-
-        FormLayout formLayout = new FormLayout();
-        formLayout.addFormItem(pageLimitSelect, "Size");
-        formLayout.setSizeFull();
-
-        pageLimitSelect.setValue(100);
-        pageLimitSelect.setItems(100, 200, 300, 400, 500);
-
-        pageLimitSelect.addValueChangeListener(event -> {
-            Integer size = event.getValue();
-            vehicleService.setPageSize(size);
-            vehicleService.submitForm();
-        });
-
-        return new HorizontalLayout(prevButton, nextButton, formLayout);
+        headerRow.getCell(markColumn).setComponent(
+                createFilterHeader("Marka", vehicleFilter::setMark));
+        headerRow.getCell(modelColumn).setComponent(
+                createFilterHeader("Model", vehicleFilter::setModel));
+        headerRow.getCell(yearColumn).setComponent(
+                createFilterHeader("Rok produkcji", vehicleFilter::setYear));
+        headerRow.getCell(registrationDate).setComponent(
+                createFilterHeader("Data pierwszej rejestracji ", vehicleFilter::setDate));
     }
 
     private static class VehicleFilter {
