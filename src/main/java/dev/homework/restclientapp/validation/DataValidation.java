aedcb14397c46db1fa9c394ad46fc04b93a46b5c
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-29 18:31:22 +0100

    Changed validation and added new one for validating the type of date field

--- src/main/java/dev/homework/restclientapp/validation/DataValidation.java
+++ src/main/java/dev/homework/restclientapp/validation/DataValidation.java
@@ -1,12 +1,16 @@
 package dev.homework.restclientapp.validation;
 
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.data.binder.Binder;
 import dev.homework.restclientapp.dto.request.VehicleRequest;
+import dev.homework.restclientapp.vaadin.notification.FormValidationNotification;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+
+import static org.reflections.Reflections.log;
 
 @Component
 public class DataValidation {
@@ -16,9 +20,21 @@ public class DataValidation {
     public void validateProvinceNameIsNotEmpty(Select<String> provinces) {
 
         binder.forField(provinces)
-                .asRequired("Wybierz województwo!")
-                .bind(VehicleRequest::getProvinceName, VehicleRequest::setProvinceName)
-                .validate();
+                .asRequired("Pole nie może być puste!")
+                .bind(VehicleRequest::getProvinceName, VehicleRequest::setProvinceName);
+
+        provinces.addValueChangeListener(event -> binder.validate());
+
+    }
+
+    public void validateDateTypeIsNotEmpty(ComboBox<String> typeOfDate) {
+
+        binder.forField(typeOfDate)
+                .asRequired("Pole nie może być puste!")
+                .bind(VehicleRequest::getTypeOfDate, VehicleRequest::setTypeOfDate);
+
+        typeOfDate.addValueChangeListener(event -> binder.validate());
+
     }
 
 
@@ -26,6 +42,20 @@ public class DataValidation {
 
         validateDateFrom(datePickerFrom, datePickerTo);
         validateDateTo(datePickerFrom, datePickerTo);
+    }
+
+    private void validateDateFrom(DatePicker datePickerFrom, DatePicker datePickerTo) {
+        binder.forField(datePickerFrom)
+                .withValidator(date -> date == null || datePickerTo.getValue() == null || ! date.isAfter(datePickerTo.getValue()),
+                        "Data od nie może być późniejsza niż data do!")
+                .asRequired("Wybierz datę od!")
+                .withValidator(date -> date == null || ! date.isAfter(LocalDate.now()),
+                        "Data nie może być późniejsza niż dzisiejsza data!")
+                .withConverter(LocalDate::toString, LocalDate::parse)
+                .bind(VehicleRequest::getDateFrom, VehicleRequest::setDateFrom);
+
+        datePickerFrom.addValueChangeListener(event -> binder.validate());
+
     }
 
     private void validateDateTo(DatePicker datePickerFrom, DatePicker datePickerTo) {
@@ -40,17 +70,15 @@ public class DataValidation {
         datePickerTo.addValueChangeListener(event -> binder.validate());
     }
 
-    private void validateDateFrom(DatePicker datePickerFrom, DatePicker datePickerTo) {
-        binder.forField(datePickerFrom)
-                .withValidator(date -> date == null || datePickerTo.getValue() == null || ! date.isAfter(datePickerTo.getValue()),
-                        "Data od nie może być późniejsza niż data do!")
-                .withValidator(date -> date == null || ! date.isAfter(LocalDate.now()),
-                        "Data nie może być późniejsza niż dzisiejsza data!")
-                .withConverter(LocalDate::toString, LocalDate::parse)
-                .bind(VehicleRequest::getDateFrom, VehicleRequest::setDateFrom);
 
-        datePickerFrom.addValueChangeListener(event -> binder.validate());
+    public void checkIfAllDateAreProvided(FormValidationNotification formValidationNotification) {
+        if (! binder.isValid()) {
+            formValidationNotification.showInvalidDateNotification();
+
+            binder.validate().getValidationErrors().forEach(error ->
+                    log.error("Field error: {}", error.getErrorMessage())
+            );
+        }
     }
-
 
 }
