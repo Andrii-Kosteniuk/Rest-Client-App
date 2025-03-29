48250b67f22a9da925adaa5b1560d75cd3b5d8de
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-29 18:26:52 +0100

    Use a close button component

--- src/main/java/dev/homework/restclientapp/vaadin/notification/FormValidationNotification.java
+++ src/main/java/dev/homework/restclientapp/vaadin/notification/FormValidationNotification.java
@@ -2,30 +2,26 @@ package dev.homework.restclientapp.vaadin.notification;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import dev.homework.restclientapp.vaadin.component.CloseButtonComponent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FormValidationNotification {
+public class FormValidationNotification extends Div  {
 
     public void showInvalidDateNotification() {
         Notification errorValidationNotification = new Notification();
         errorValidationNotification.addThemeVariants(NotificationVariant.LUMO_WARNING);
 
-        Div text = new Div(new Text("Please, check if you are providing a correct date!"));
+        Div notificationMessage = new Div(new Text("Please, check if you are providing a correct data!"));
 
-        Button closeButton = new Button(new Icon("lumo", "cross"));
-        closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
-        closeButton.setAriaLabel("Close");
-        closeButton.addClickListener(event -> errorValidationNotification.close());
+        Button closeNotificationButton = CloseButtonComponent.closeNotification(errorValidationNotification);
 
-        HorizontalLayout layout = new HorizontalLayout(text, closeButton);
+        HorizontalLayout layout = new HorizontalLayout(notificationMessage, closeNotificationButton);
         layout.setAlignItems(FlexComponent.Alignment.CENTER);
 
         errorValidationNotification.setDuration(5000);
