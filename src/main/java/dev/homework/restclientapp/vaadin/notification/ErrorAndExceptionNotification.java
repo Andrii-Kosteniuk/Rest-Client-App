b24dfb64ebae78aa30114c805b2cd191bfdf882d
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2025-03-29 18:26:19 +0100

    Changed error and exception notification class

--- src/main/java/dev/homework/restclientapp/vaadin/notification/ErrorAndExceptionNotification.java
+++ src/main/java/dev/homework/restclientapp/vaadin/notification/ErrorAndExceptionNotification.java
@@ -1,25 +1,44 @@
 package dev.homework.restclientapp.vaadin.notification;
 
-
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
-import org.springframework.stereotype.Component;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import dev.homework.restclientapp.vaadin.component.CloseButtonComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Component
-public class ErrorAndExceptionNotification {
+public class ErrorAndExceptionNotification extends Div {
 
-    public void showNotificationErrorIfTimeOutExceptionOccur(Exception ex) {
-        Div text = new Div(
-                new Paragraph(ex.getMessage()),
-                new Paragraph("Try to reload page, please!"));
-        Notification notification = new Notification(text);
+    private static final Logger log = LoggerFactory.getLogger(ErrorAndExceptionNotification.class);
 
-        notification.setDuration(5000);
-        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+    public static void showNotificationTimeOutExceptionOccur(Exception ex) {
+        UI ui = UI.getCurrent();
+        if (ui != null) {
+            ui.access(() -> {
+                Notification errorTimeOutNotification = new Notification();
+                errorTimeOutNotification.addThemeVariants(NotificationVariant.LUMO_ERROR);
 
-        notification.open();
+                Div text = new Div(
+                        new Paragraph(ex.getMessage()),
+                        new Paragraph("Spróbuj przeładować stronę, proszę!"));
+
+                Button closeNotificationButton = CloseButtonComponent.closeNotification(errorTimeOutNotification);
+                HorizontalLayout layout = new HorizontalLayout(text, closeNotificationButton);
+                layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+                errorTimeOutNotification.add(layout);
+
+                errorTimeOutNotification.open();
+            });
+        } else {
+            log.error("UI instance is not available. Cannot show notification.");
+        }
+
     }
 
 }
